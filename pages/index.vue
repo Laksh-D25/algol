3ae78c1c21e276c567dcfb2d3f2e552763aca035
Laksh-D25 <laaaksh25@gmail.com>
2025-01-25 03:37:13 +0530

    Checking vercel app deployment

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="bg-blue-500 text-2xl text-center">
         Hello
+        <Button :label="count.value"></Button>
     </div>
 </template>
+
+<script setup>
+import { Button } from 'primevue';
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const onClicked = () => {
+    count.value++;
+}
+</script>
